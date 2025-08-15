726647547701235430900540924cb5800b004bc9
Aditya Eka Pranata <aepranata@gmail.com>
2025-08-15 09:25:46 +0700

    fs: Include KernelSU
    Change-Id: I4dcfa18e706bc1c59e725eaa97686a0ab23df3a6
    Signed-off-by: Aditya Eka Pranata <aepranata@gmail.com>

--- fs/stat.c
+++ fs/stat.c
@@ -18,6 +18,10 @@
 #include <asm/uaccess.h>
 #include <asm/unistd.h>
 
+#ifdef CONFIG_KSU
+#include <linux/ksu.h>
+#endif
+
 void generic_fillattr(struct inode *inode, struct kstat *stat)
 {
 	stat->dev = inode->i_sb->s_dev;
@@ -87,12 +91,21 @@ int vfs_fstat(unsigned int fd, struct kstat *stat)
 }
 EXPORT_SYMBOL(vfs_fstat);
 
+#ifdef CONFIG_KSU
+extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
+
 int vfs_fstatat(int dfd, const char __user *filename, struct kstat *stat,
 		int flag)
 {
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = 0;
+
+#ifdef COFIG_KSU
+	if (get_ksu_state() > 0)
+		ksu_handle_stat(&dfd, &filename, &flag);
+#endif
 
 	if ((flag & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		      AT_EMPTY_PATH)) != 0)
